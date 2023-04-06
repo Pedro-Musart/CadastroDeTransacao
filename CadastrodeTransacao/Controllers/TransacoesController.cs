6b8ce1f6f2ea0e2f4c39d76ffd3bf2de9ba6d7d7
Pedro Soares <pedrowesleyfs@gmail.com>
2023-04-06 11:21:04 -0300

    Add project files.

---
+++ CadastrodeTransacao/Controllers/TransacoesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CadastrodeTransacao.Controllers
+{
+    public class TransacoesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CadastrodeTransacao/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CadastrodeTransacao.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
